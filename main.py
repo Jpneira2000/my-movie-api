cccf94871cca72bb5110d350a501e5a114990441
jpneira2000 <jpneira2000@gmail.com>
2023-12-03 21:41:36 -0600

    Clase 'Método GET'

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = 'Mi aplicación con FastAPI'
 app.version = '0.0.1'
 
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planera llamado Pandora viven los Na'Vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'
+    }
+]
+
 @app.get('/', tags=['home'])
 def message():
-    return 'Hello, World!'
+    return HTMLResponse('<h1>Hello, World</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
